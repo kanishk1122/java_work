a9800ef5db3984bd7b502cb83190ab2b395fdbad
kanishk1122 <kansihk21soni@gmail.com>
2024-03-03 15:01:10 +0530

    first commit

---
+++ Ch_04_libraral.java
@@ -0,0 +1,12 @@
+public class Ch_04_libraral {
+    public static void main(String[] args) {
+        byte age =1;
+        char name ='k';
+        float hight = 5.6f;
+        System.out.println("age was : "+ age+ "\n"+",start name with :" + name +"\n"+"hieght was : " + hight);
+
+
+
+
+    }
+}
