8df48e4b84958f4e6d4ba65045400915657d50d3
kanishk1122 <kansihk21soni@gmail.com>
2024-03-03 16:06:04 +0530

    hello

--- Oops.java
+++ Oops.java
@@ -1,7 +1,32 @@
-public class Oops {
-    public static void main (String[] args){
 
+ class employee2{
+    private int id;
+    private String name;
+    public String getname(){
+     return name;
+    }
+    public void setname(String n){
+        name=n;
+    }
+    public int get(int i){
+       return id;
 
     }
+    public void set(int i){
+        id=i;
+    }
 
+
+    public void printd(){
+        System.out.println("my id is : " +id);
+        System.out.println("my name is : " + name );
+    }
+}
+public class Oops {
+    public static void main (String[] args){
+     employee2 name = new employee2();
+     name.setname("kansihk");
+     //print the attributes
+      name.printd();
+    }
 }
